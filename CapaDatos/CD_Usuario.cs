a9188a5285da160966f75ae80f31c38c84ab2dd9
Jvven <jevenssenmartinezm24@gmail.com>
2026-03-06 16:29:09 -0400

    Nuevas clases Usuarios

---
+++ CapaDatos/CD_Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Usuario
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+    
+    
+
+    public DataTable Mostrar()
+    {    
+            comando.Connection = conexion.ObtenerConexion();
+            comando.CommandText = "MostrarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+        
+    }
+}
---
+++ CapaNegocio/CN_Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CN_Usuario
+    {
+        private CD_Usuario objetoCD = new CD_Usuario();
+
+        public DataTable MostrarUsuario()
+        {
+            DataTable tablaUsuario = new DataTable();
+            tablaUsuario = objetoCD.Mostrar();
+            return tablaUsuario;
+        }
+    }
+}
